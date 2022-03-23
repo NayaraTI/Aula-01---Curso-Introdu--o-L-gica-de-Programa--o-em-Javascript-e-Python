51ad6d8018ebbc634aa80b7e502d76668e2fec12
nayara <naydossantos@yahoo.com.br>
2022-03-23 16:05:08 -0300

    first commit

---
+++ aula01-1.py
@@ -0,0 +1,35 @@
+print("Olá")
+
+
+# Comentário em 1 linha
+
+"""
+
+Aqui
+Comentario
+Bloco de Codigo
+
+
+"""
+## Tipagem forte
+
+#print(2+'2')
+
+print(type("Joao'"))
+
+print("A minha idade é: ",34)
+print("A soma das nossas idades é: ",(25+36+27))
+print("A média das nossas idades é: ",(25+36+27)/3)
+
+nome_cidade = "Floripa"
+print(nome_cidade)
+
+
+
+num1 = 10
+num2 = 20
+num3 = 30
+
+### = Atribuição
+# == Igualdade ou comparação
+
